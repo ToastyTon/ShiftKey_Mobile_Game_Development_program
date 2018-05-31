ee03fd09e10fe876959dedcda8ac8deb90d5ca5d
jackzhou77571 <jackzhou77571@gmail.com>
2018-05-31 16:20:58 -0300

    2.start code

---
+++ Game Objects and Scripts/Assets/Clock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    // Use this for initialization
+
+    void Awake()
+    {
+        Debug.Log(Time.time); 
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
