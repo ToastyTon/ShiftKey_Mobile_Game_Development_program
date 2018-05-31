07d8e6dbf455731588cb83304a732bda39dec8eb
jackzhou77571 <jackzhou77571@gmail.com>
2018-05-31 16:58:00 -0300

    4-finish the clock program

--- Game Objects and Scripts/Assets/Clock.cs
+++ Game Objects and Scripts/Assets/Clock.cs
@@ -7,7 +7,9 @@ public class Clock : MonoBehaviour {
     public Transform hoursTransform;
     public Transform minutesTransform;
     public Transform secondsTransform;
-    // Use this for initialization
+
+    public bool continuous;
+   
 
     const float degreesPerHour = 30f, degreesPerMinute = 6f, degreesPersecond = 6f;
     void Awake()
@@ -24,6 +26,35 @@ public class Clock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (continuous)
+        {
+            UpdateContinuous();
+        }
+        else
+        {
+            UpdateDiscreate();
+        }
+
+        /*DateTime time = DateTime.Now;
+        hoursTransform.localRotation =  Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPersecond, 0f);
+        */
+    }
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPersecond, 0f);
+    }
+
+    void UpdateDiscreate()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPersecond, 0f);
+    }
 }
