bd8bf2fbad8288529caff0456909b9f9baba486b
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-01 11:40:06 -0300

    2.0 Building a Graph Let's start

---
+++ Building a Graph/Assets/Graph.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+    public Transform pointPrefab;
+    // Use this for initialization
+    void Awake()
+    {
+     Transform point =   Instantiate(pointPrefab);
+        point.localPosition = Vector3.right;
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
