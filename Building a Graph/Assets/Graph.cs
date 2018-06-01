ce8067fcde122dcb5f0e3c81c9059f959d954cab
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-01 11:54:29 -0300

    2.1Concise Syntax

--- Building a Graph/Assets/Graph.cs
+++ Building a Graph/Assets/Graph.cs
@@ -7,14 +7,21 @@ public class Graph : MonoBehaviour {
     // Use this for initialization
     void Awake()
     {
-     Transform point =   Instantiate(pointPrefab);
-        point.localPosition = Vector3.right;
+        for (int i = 0; i < 10; i++)
+        {
+            //i = i + 1;
+            Transform point = Instantiate(pointPrefab);
+            point.localPosition = Vector3.right*i;
+            point.localScale = Vector3.one / 5f;
+        }
+       
+       // point.localPosition = Vector3.right * 2f;
     }
     void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
 		
 	}
