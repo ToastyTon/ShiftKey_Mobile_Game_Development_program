b6299ff5bc7d89bb7f5a3bf9801445d8246e9e01
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-01 18:47:28 -0300

    3.0 Showing Something

---
+++ Constructing a Fractal/Assets/Fractal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+    public Mesh mesh;
+    public Material material;
+	// Use this for initialization
+	void Start () {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
