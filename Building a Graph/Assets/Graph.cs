565d4cb405c74479c03fb7c87004571e6062be6b
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-01 14:09:59 -0300

    2.5  Showing a Sine Wave

--- Building a Graph/Assets/Graph.cs
+++ Building a Graph/Assets/Graph.cs
@@ -4,30 +4,37 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour {
     public Transform pointPrefab;
+    Transform[] points;
 
     [Range(10,100)]
     public int resolution = 10;
+
+    Vector3 position;
     // Use this for initialization
     void Awake()
     {
         float step = 2f / resolution;
-
+        
+        points = new Transform[resolution];
+       
         Vector3 scale = Vector3.one *step;
-        Vector3 position = Vector3.zero;
+        
         position.x = 0f;
         position.z = 0f;
-        for (int i = 0; i < resolution; i++)
+
+        for (int i = 0; i < points.Length; i++)
         {
             //i = i + 1;
             Transform point = Instantiate(pointPrefab);
             //point.localPosition = Vector3.right*((i+0.5f)/5f-1f);
             position.x = (i + 0.5f) *step - 1f;
-            position.y = position.y = position.x * position.x * position.x;
-
+           
             point.localPosition = position;
             //point.localScale = Vector3.one / 5f;
             point.localScale = scale;
             point.SetParent(transform,false);
+
+            points[i] = point;
         }
        
        // point.localPosition = Vector3.right * 2f;
@@ -38,6 +45,18 @@ public class Graph : MonoBehaviour {
 	
 	
 	void Update () {
-		
+
+        position.y = Mathf.Sin(position.x);
+        position.z = 0f;
+
+
+        for (int i = 0; i < points.Length; i++)
+        {
+
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
 	}
 }
