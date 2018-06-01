b4fcba49d59182fba5612a50358a3560db456c76
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-01 12:27:18 -0300

    2.2Using X to Define Y

--- Building a Graph/Assets/Graph.cs
+++ Building a Graph/Assets/Graph.cs
@@ -7,12 +7,20 @@ public class Graph : MonoBehaviour {
     // Use this for initialization
     void Awake()
     {
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position = Vector3.zero;
+        position.x = 0f;
+        position.z = 0f;
         for (int i = 0; i < 10; i++)
         {
             //i = i + 1;
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right*i;
-            point.localScale = Vector3.one / 5f;
+            //point.localPosition = Vector3.right*((i+0.5f)/5f-1f);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            //point.localScale = Vector3.one / 5f;
+            point.localScale = scale;
         }
        
        // point.localPosition = Vector3.right * 2f;
