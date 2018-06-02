cd3c633b62cf6a6c3db7bd963571e24d83aa305e
jackzhou77571 <jackzhou77571@gmail.com>
2018-06-02 13:11:29 -0300

    4.0 Frames Per Second start

---
+++ Frames Per Second/Assets/Nucleon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour {
+    public float attractionForce;
+
+    Rigidbody body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        body.AddForce(transform.localPosition * -attractionForce);
+	}
+}
